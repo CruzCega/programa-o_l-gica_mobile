8f72b1e452181c7d0b9e4b715dbdbc399d9772c4
Rodrigo Noronha <rodrigo.c.noronha.m@gmail.com>
2024-04-11 21:49:11 -0300

    Update. Python
    Fiz a atividade do dia 11

---
+++ aula 1/aula02.py
@@ -0,0 +1,10 @@
+#print("olá, mundo finalmente quebrei a maldição")
+
+#quando eu faço uma comentario simples
+
+print("olá, bem vindo ao nosso app!")
+nome = input("qual é o seu nome?:")
+idade = input("qual a sua idade?:")
+gosto = input("qual é o seu hobbie?:")
+
+print(f"olá {nome}, você tem {idade} anos, gosta de {gosto}!:")
